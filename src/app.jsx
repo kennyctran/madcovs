a3b90ec709e7304e00342bfb9f947c7fd8c00193
Kenny Tran <kenny.tran138@gmail.com>
2021-06-11 11:04:05 -0400

    Render parsed madLibs to screen

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import useParser from "./custom/useParser";
+import MadLib from './components/MadLib';
+import extractMadLibs from './helpers/extractMadLibs';
+// On button submit
+  // Move user to a new page using react router
+  // That page will require the coverLetter variable
+  // It will need to  parse the coverLetter for madLibs first
+  // It will need to display the split screen view
 
 function App() {
-  const [parsed, setParsed] = useParser("");
+  const [coverLetter, setCoverLetter] = useParser("");
+  const [madLibs, setMadLibs] = useState([]);
+
+  const handleSubmit = () => {
+    const extractedLibs = extractMadLibs(coverLetter, '_');
+    console.log(extractedLibs)
+    setMadLibs(extractedLibs);
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
@@ -12,10 +27,18 @@ function App() {
           name="coverLetter"
           id="coverLetter"
           data-testid="coverInput"
-          onChange={(e) => setParsed(e.target.value)}
+          onChange={(e) => setCoverLetter(e.target.value)}
         />
+        <button type="button" onClick={handleSubmit}>submit</button>
       </form>
-      <div className="parsed">{parsed}</div>
+      <div>
+        {madLibs.map((lib, i) => {
+          return (<MadLib
+            key={i}
+            text={lib}
+          />);
+        })}
+      </div>
     </div>
   );
 }
